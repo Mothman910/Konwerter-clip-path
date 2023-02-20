43e4e034a93a624dfddba7aabbe3f98f561b4373
Mothman910 <fijalkowski.adam@outlook.com>
2023-02-20 17:00:24 +0100

    Działający konwerter

--- JS/Konwerter clip-path.js
+++ JS/Konwerter clip-path.js
@@ -1,46 +1,38 @@
-const readline = require("readline");
+const form = document.querySelector("#form");
+form.addEventListener("submit", handleSubmit);
+//Funkcja obsługi formularza
+function handleSubmit(event) {
+    event.preventDefault(); // zapobiega przeładowaniu strony po wysłaniu formularza
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+    const polygonPoints = document.querySelector("#polygonPoints").value;
+    const viewBoxWidth = document.querySelector("#viewBoxWidth").value;
+    const viewBoxHeight = document.querySelector("#viewBoxHeight").value;
 
-rl.question(
-    'Podaj punkty w formacie "x1 y1 x2 y2 ... xn yn": ',
-    (polygonPoints) => {
-        rl.question("Podaj szerokość viewBox: ", (viewBoxWidth) => {
-            rl.question("Podaj wysokość viewBox: ", (viewBoxHeight) => {
-                // Rozdzielenie punktów na osobne wartości
-                const pointsArray = polygonPoints.split(" ");
+    const pointsArray = polygonPoints.split(" ");
+    const pointsPercentArray = pointsArray.map((point, index) => {
+        if (index % 2 === 0) {
+            return ((point / viewBoxWidth) * 100).toFixed(2) + "%";
+        } else {
+            return ((point / viewBoxHeight) * 100).toFixed(2) + "%";
+        }
+    });
 
-                // Konwersja wartości pikselowych na wartości procentowe
-                const pointsPercentArray = pointsArray.map((point, index) => {
-                    // Konwersja wartości X
-                    if (index % 2 === 0) {
-                        return ((point / viewBoxWidth) * 100).toFixed(2) + "%";
-                    }
-                    // Konwersja wartości Y
-                    else {
-                        return ((point / viewBoxHeight) * 100).toFixed(2) + "%";
-                    }
-                });
-
-                // Połączenie punktów w ciąg procentowy z przecinkami między punktami
-                let polygonPointsPercent = "";
-                for (let i = 0; i < pointsPercentArray.length; i += 2) {
-                    polygonPointsPercent +=
-                        pointsPercentArray[i] + " " + pointsPercentArray[i + 1];
-                    if (i < pointsPercentArray.length - 2) {
-                        polygonPointsPercent += ", ";
-                    }
-                }
-
-                console.log(polygonPointsPercent);
-
-                rl.question("Wciśnij ENTER, aby zakończyć...", () => {
-                    rl.close();
-                });
-            });
-        });
+    let polygonPointsPercent = "";
+    for (let i = 0; i < pointsPercentArray.length; i += 2) {
+        polygonPointsPercent +=
+            pointsPercentArray[i] + " " + pointsPercentArray[i + 1];
+        if (i < pointsPercentArray.length - 2) {
+            polygonPointsPercent += ", ";
+        }
     }
-);
+
+    //  return(polygonPointsPercent);
+
+    // utworzenie nowego elementu HTML i wstawienie go na stronę
+    //  resultDiv.textContent = polygonPointsPercent;
+
+    //const resultDiv = return(polygonPointsPercent);
+    //document.getElementById("result").innerHTML = resultDiv;
+    document.getElementById("result").innerHTML = polygonPointsPercent;
+//    event.preventDefault(); // zapobiega przeładowaniu strony po wysłaniu formularza
+}
